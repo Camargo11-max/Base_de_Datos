8fa9ed0b4c9ad941c38e7b222c3a20cb0b672172
Camargo11-max <cristiancamargo001@gmail.com>
2025-02-11 21:00:02 -0500

    Update Cerveceria.sql

--- Cerveceria.sql
+++ Cerveceria.sql
@@ -2,50 +2,56 @@ CREATE DATABASE cerveceria;
 USE cerveceria;
 
 CREATE TABLE materia_prima (
-    id_materiap INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_mp varchar(40) NOT NULL,
-    color_mp  varchar(20),
-    sabor_mp varchar(30),
-    textura_mp varchar(40),
-    aroma_mp varchar(30)
+    id_materiap INT NOT NULL AUTO_INCREMENT,
+    nombre_mp VARCHAR(30) NOT NULL,
+    color_mp  VARCHAR(30) NOT NULL,
+    sabor_mp  VARCHAR(30) NOT NULL,
+    textura_mp VARCHAR(30) NOT NULL,
+    aroma_mp VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id_materiap`)
 );
 
 CREATE TABLE cerveza_matprima (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_cer_matprima INT NOT NULL AUTO_INCREMENT,
     cantidad INT NOT NULL,
     unidad INT NOT NULL,
     id_materiap INT NOT NULL,
     id_cerveza INT NOT NULL,
-    FOREING KEY (id_materiap) REFERENCES materia_prima (id_materiap), 
-    FOREING KEY (id_cerveza) REFERENCES materia_prima (id_cerveza)
+    PRIMARY KEY (`id_cer_matprima`),
+    FOREIGN KEY (`id_materiap`) REFERENCES `cerveceria`.`materia_prima` (`id_materiap`),
+    FOREIGN KEY (`id_cerveza`) REFERENCES `cerveceria`.`cerveza` (`id_cerveza`)
 );
 
 CREATE TABLE cerveza (
-    id_cerveza INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_cer varchar(40) NOT NULL,
-    color_cer  varchar(20),
-    sabor_cer varchar(30),
-    textura_cer varchar(40),
-    aroma_cer varchar(30),
+    id_cerveza INT NOT NULL AUTO_INCREMENT,
+    nombre_cer VARCHAR(30) NOT NULL,
+    color_cer  VARCHAR(30) NOT NULL,
+    sabor_cer VARCHAR(30) NOT NULL,
+    textura_cer VARCHAR(30) NOT NULL,
+    aroma_cer VARCHAR(30) NOT NULL,
     id_categoria INT NOT NULL,
-    FOREING KEY (id_categoria) REFERENCES categoria (id_categoria)
+    PRIMARY KEY (`id_cerveza`),
+    FOREIGN KEY (`id_categoria`) REFERENCES `cerveceria`.`categoria` (`id_categoria`)
 );
 
 CREATE TABLE categoria (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_cat varchar (30) NOT NULL ,
-    detalle varchar (40) NOT NULL
+    id_categoria INT NOT NULL AUTO_INCREMENT,
+    nombre_cat VARCHAR(30) NOT NULL ,
+    detalle VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id_categoria`)
 );
 
 CREATE TABLE tipo_categoria (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_categoria INT NOT NULL,
+    id_tipo_cat INT NOT NULL AUTO_INCREMENT,
+    nombre_tipo_cat VARCHAR(30) NOT NULL,
+    categoria_id_categoria INT NOT NULL,
     id_tipo INT NOT NULL,
-    FOREING KEY (id_tipo) REFERENCES tipo (id_tipo),
-    FOREING KEY (id_categoria) REFERENCES categoria (id_categoria)
+    FOREIGN KEY (`categoria_id_categoria`) REFERENCES `cerveceria`.`categoria` (`id_categoria`),
+    FOREIGN KEY (`id_tipo`) REFERENCES `cerveceria`.`tipo` (`id_tipo`)
 );
 
 CREATE TABLE tipo (
-    id_tipo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_tipo varchar(30) NOT NULL
+    id_tipo INT NOT NULL AUTO_INCREMENT,
+    nombre_tipo VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id_tipo`)
 );
